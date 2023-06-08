9d530a82775fe773c32f5736668eadfe42771574
Ironladd <andredsouza15@gmail.com>
2023-06-07 20:02:25 -0400

    Created endpoint for sending club win data

--- src/main/java/com/BrawlService/Controller/BattleWinController.java
+++ src/main/java/com/BrawlService/Controller/BattleWinController.java
@@ -1,14 +1,16 @@
 package com.BrawlService.Controller;
 
 
-import com.BrawlService.Business.Colour;
+
 import com.BrawlService.Business.FilterProgram;
 import com.BrawlService.Entity.StatEntity.BattleWin;
 import com.BrawlService.Service.BattleWinService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 
@@ -28,26 +30,31 @@ public class BattleWinController {
         return "update successful";
     }
 
+//    @GetMapping("/fetch")
+//    public String showWins(){
+//
+//        StringBuilder data = new StringBuilder();
+//
+//        List<BattleWin> wins = service.getBattleHistory();
+//
+//        for (BattleWin win: wins){
+//            double winRate = (double) (win.getWins().get(0) + win.getWins().get(1) + win.getWins().get(2)) / win.getWins().get(4);
+//            data    .append(win.getPlayer().getName())
+//                    .append(" | 3v3 victories: ").append(win.getWins().get(0))
+//                    .append(" | Solo victories: ").append(win.getWins().get(1))
+//                    .append(" | Duels: ").append(win.getWins().get(2))
+//                   .append(" | Duos: ").append(win.getWins().get(3))
+//                    .append(" | W/R: ").append(winRate).append("\n");
+//        }
+//
+//        new FilterProgram().printVictoryResults((ArrayList<BattleWin>) wins);
+//
+//        return data.toString();
+//    }
+
     @GetMapping("/fetch")
-    public String showWins(){
-
-        StringBuilder data = new StringBuilder();
-
-        List<BattleWin> wins = service.getBattleHistory();
-
-        for (BattleWin win: wins){
-            double winRate = (double) (win.getWins().get(0) + win.getWins().get(1) + win.getWins().get(2)) / win.getWins().get(4);
-            data    .append(win.getPlayer().getName())
-                    .append(" | 3v3 victories: ").append(win.getWins().get(0))
-                    .append(" | Solo victories: ").append(win.getWins().get(1))
-                    .append(" | Duels: ").append(win.getWins().get(2))
-                   .append(" | Duos: ").append(win.getWins().get(3))
-                    .append(" | W/R: ").append(winRate).append("\n");
-        }
-
-        new FilterProgram().printVictoryResults((ArrayList<BattleWin>) wins);
-
-        return data.toString();
+    public ResponseEntity<List<BattleWin>>getClubWinHistory(){
+        return new ResponseEntity<>(service.getBattleHistory(), HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/find/{playerTag}")
